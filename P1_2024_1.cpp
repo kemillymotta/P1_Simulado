f4025e22790c920ccdb655ecf66c2aa70135eae7
Claudio Nunes <claudio@metasistemas.com>
2024-04-08 20:41:30 -0300

    Add project files.

---
+++ P1_2024_1.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+
+// definicao de tipo
+struct NO {
+	int valor;
+	NO* prox;
+};
+
+NO* primeiro = NULL;
+
+// headers
+void menu();
+void inicializar();
+void exibirQuantidadeElementos();
+void exibirElementos();
+void inserirElemento();
+
+//--------------------------
+
+
+int main()
+{
+	menu();
+}
+
+void menu()
+{
+	int op = 0;
+	while (op != 5) {
+		system("cls"); // somente no windows
+		cout << "Menu Lista Ligada - P1";
+		cout << endl << endl;
+		cout << "1 - Inicializar Lista \n";
+		cout << "2 - Exibir quantidade de elementos \n";
+		cout << "3 - Exibir elementos \n";
+		cout << "4 - Inserir elemento \n";
+		cout << "5 - Sair \n\n";
+
+		cout << "Opcao: ";
+		cin >> op;
+
+		switch (op)
+		{
+		case 1: inicializar();
+			break;
+		case 2: exibirQuantidadeElementos();
+			break;
+		case 3: exibirElementos();
+			break;
+		case 4:  inserirElemento();
+			break;
+		case 5:
+			return;
+		default:
+			break;
+		}
+
+		system("pause"); // somente no windows
+	}
+}
+
+void inicializar()
+{
+	// se a lista já possuir elementos
+// libera a memoria ocupada
+	NO* aux = primeiro;
+	while (aux != NULL) {
+		NO* paraExcluir = aux;
+		aux = aux->prox;
+		free(paraExcluir);
+	}
+
+	primeiro = NULL;
+	cout << "Lista inicializada \n";
+
+}
+
+void exibirQuantidadeElementos() {
+
+	int nElementos = 0;
+	NO* aux = primeiro;
+	while (aux != NULL) {
+		nElementos++;
+		aux = aux->prox;
+	}
+	cout << "Quantidade de elementos: " << nElementos << endl;
+
+}
+
+void exibirElementos()
+{
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	else {
+		cout << "Elementos: \n";
+		NO* aux = primeiro;
+		while (aux != NULL) {
+			cout << aux->valor << endl;
+			aux = aux->prox;
+		}
+	}
+}
+
+void inserirElemento()
+{
+
+}
+
+
