a6d4c5898673b9aefb3306ea2b1c286bd6c85925
Claudio Nunes <claudio@metasistemas.com>
2025-03-31 18:22:34 -0300

    Ajustes para a P1 de 2025

--- P1_2025_1.cpp
+++ P1_2025_1.cpp
@@ -15,6 +15,9 @@ void inicializar();
 void exibirQuantidadeElementos();
 void exibirElementos();
 void inserirElemento();
+void removerElemento();
+void buscarElemento();
+void exibirMenorValor();
 
 //--------------------------
 
@@ -27,7 +30,7 @@ int main()
 void menu()
 {
 	int op = 0;
-	while (op != 5) {
+	while (op != 8) {
 		system("cls"); // somente no windows
 		cout << "Menu Lista Ligada - P1";
 		cout << endl << endl;
@@ -35,7 +38,10 @@ void menu()
 		cout << "2 - Exibir quantidade de elementos \n";
 		cout << "3 - Exibir elementos \n";
 		cout << "4 - Inserir elemento \n";
-		cout << "5 - Sair \n\n";
+		cout << "5 - Remover elemento \n";
+		cout << "6 - Buscar elemento \n";
+		cout << "7 - Exibir menor valor \n";
+		cout << "8 - Sair \n\n";
 
 		cout << "Opcao: ";
 		cin >> op;
@@ -50,20 +56,28 @@ void menu()
 			break;
 		case 4:  inserirElemento();
 			break;
-		case 5:
+		case 5:  removerElemento();
+			break;
+		case 6:
+			buscarElemento();
+			break;
+
+		case 7:
+			exibirMenorValor();
 			return;
 		default:
 			break;
 		}
-
 		system("pause"); // somente no windows
 	}
 }
 
 void inicializar()
 {
+
 	// se a lista já possuir elementos
-// libera a memoria ocupada
+	// libera a memoria ocupada
+
 	NO* aux = primeiro;
 	while (aux != NULL) {
 		NO* paraExcluir = aux;
@@ -113,6 +127,72 @@ void inserirElemento()
 		return;
 	}
 
+	// solicita o valor do novo elemento
+	cout << "Digite o valor do novo elemento: ";
+	cin >> novo->valor;
+	novo->prox = NULL;
+
+	// verifica se o elemento já existe na lista
+	NO* aux = primeiro;
+	while (aux != NULL) {
+		if (aux->valor == novo->valor) {
+			cout << "Elemento duplicado \n";
+			free(novo);
+			return;
+		}
+		aux = aux->prox;
+	}
+
+	// insere o novo elemento no final da lista
+	if (primeiro == NULL) {
+		primeiro = novo;
+	}
+	else {
+		aux = primeiro;
+		while (aux->prox != NULL) {
+			aux = aux->prox;
+		}
+		aux->prox = novo;
+	}
+
+	cout << "Elemento inserido \n";
 }
 
+void removerElemento() {
+	if (primeiro == NULL) {
+		cout << "Lista vazia \n";
+		return;
+	}
+	int valor;
+	cout << "Digite o valor do elemento a ser removido: ";
+	cin >> valor;
+	NO* aux = primeiro;
+	NO* anterior = NULL;
+	while (aux != NULL) {
+		if (aux->valor == valor) {
+			if (anterior == NULL) {
+				primeiro = aux->prox;
+			}
+			else {
+				anterior->prox = aux->prox;
+			}
+			free(aux);
+			cout << "Elemento removido \n";
+			return;
+		}
+		anterior = aux;
+		aux = aux->prox;
+	}
+	cout << "Elemento nao encontrado \n";
+}
 
+void buscarElemento()
+{
+
+
+}
+
+void exibirMenorValor()
+{
+
+}
